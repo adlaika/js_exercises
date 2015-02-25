acbf83b81389674681d1231f3a935a5d5e4bd8c4
docmoxie <michaelaarnold@gmail.com>
2015-02-24 18:40:55 -0800

    added ch 10 exercises

---
+++ eloquent_js/10_modules/10_modules.js
@@ -0,0 +1,17 @@
+//Chapter 10: Modules
+//Exercises
+
+//Month Names
+var month = function() {
+  var names = ["January", "February", "March", "April", "May", "June",
+               "July", "August", "September", "October", "November", "December"];
+  return {
+    name: function(number) { return names[number]; },
+    number: function(name) { return names.indexOf(name); }
+  };
+}();
+
+console.log(month.name(2));
+// → March
+console.log(month.number("November"));
+// → 10
